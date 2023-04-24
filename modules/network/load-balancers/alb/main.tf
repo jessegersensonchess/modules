15baf7e0692af88692d45a173f03ed5b6c58d1a5
jessegersensonchess <zippy@localhost.localdomain>
2023-04-24 15:04:25 +0200

    sync

--- modules/network/load-balancers/alb/main.tf
+++ modules/network/load-balancers/alb/main.tf
@@ -1,17 +1,17 @@
 resource "aws_lb" "lb" {
   name                             = upper("alb-${var.service}")
-  desync_mitigation_mode           = "defensive"
-  drop_invalid_header_fields       = "false"
-  enable_cross_zone_load_balancing = "true"
+  desync_mitigation_mode           = var.desync_mitigation_mode
+  drop_invalid_header_fields       = var.drop_invalid_header_fields
+  enable_cross_zone_load_balancing = var.enable_cross_zone_load_balancing
   enable_deletion_protection       = var.enable_deletion_protection
-  enable_http2                     = "true"
-  enable_waf_fail_open             = "false"
-  idle_timeout                     = var.lb_idle_timeout # "60"
-  internal                         = "false"
-  ip_address_type                  = "ipv4"
-  load_balancer_type               = "application"
+  enable_http2                     = var.enable_http2
+  enable_waf_fail_open             = var.enable_waf_fail_open
+  idle_timeout                     = var.lb_idle_timeout
+  internal                         = var.internal
+  ip_address_type                  = var.ip_address_type
+  load_balancer_type               = var.load_balancer_type
 
-  preserve_host_header = "false"
+  preserve_host_header = var.preserve_host_header
   security_groups      = var.security-groups
 
   subnet_mapping {
@@ -21,8 +21,9 @@ resource "aws_lb" "lb" {
   subnet_mapping {
     subnet_id = var.subnet_id_b
   }
+  
   tags = {
-    Managed_By = "Terraform"
+    Managed_By = var.managed_by
     Owner      = var.owner
     Service    = var.service
     Env        = var.environment
--- modules/network/load-balancers/alb/variables.tf
+++ modules/network/load-balancers/alb/variables.tf
@@ -1,3 +1,53 @@
+variable "ip_address_type" {
+	type = string
+	default = "ipv4"
+	}
+
+variable "internal" {
+	type = bool
+	default = false
+	}
+
+variable "enable_waf_fail_open" {
+	type = bool
+	default = false
+	}
+
+variable "desync_mitigation_mode" {
+	type = string
+	default = "defensive"
+	}
+
+variable "enable_http2" {
+	type = bool
+	default = true
+	}
+
+variable "enable_cross_zone_load_balancing" {
+	type = bool
+	default = true
+	}
+
+variable "drop_invalid_header_fields" {
+	type = bool
+	default = false
+	}
+
+variable "preserve_host_header" {
+	type = bool
+	default = false
+	}
+
+variable "load_balancer_type" {
+	type = string
+	default = "application"
+	}
+
+variable "managed_by" {
+	type = string
+	default = "Terraform"
+	}
+	
 variable "subnet_id_a" {
   type    = string
   default = "my-subnet-a"
