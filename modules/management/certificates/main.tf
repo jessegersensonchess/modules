d4746813c0a606209464aab0c36b6d9f9d35b9a1
jessegersensonchess <zippy@localhost.localdomain>
2023-05-01 12:17:47 +0200

    sync

---
+++ modules/management/certificates/main.tf
@@ -0,0 +1,30 @@
+resource "aws_acm_certificate" "ssl-certificate" {
+  domain_name   = var.domain-name
+  key_algorithm = var.key-algorithm
+
+  options {
+    certificate_transparency_logging_preference = var.logging-preference
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  subject_alternative_names = [var.domain-name]
+
+  tags = {
+    name        = var.domain-name
+    Description = var.description
+    Environment = var.environment
+    Service     = var.service
+  }
+
+  tags_all = {
+    name        = var.domain-name
+    Description = var.description
+    Environment = var.environment
+    Service     = var.service
+  }
+
+  validation_method = var.validation-method
+}
