265fb9227b0bd828fe34551f85b11e08b752b34f
jessegersensonchess <zippy@localhost.localdomain>
2023-05-04 12:02:32 +0200

    add descriptions to certificate variables

--- examples/management/certificates/outputs.tf
+++ examples/management/certificates/outputs.tf
@@ -7,7 +7,6 @@ output "ssl-certificate-domain-name" {
   #sensitive = true
 }
 
-
 output "arn" {
   value = module.ssl-certificate.arn
 }
@@ -23,20 +22,4 @@ output "status" {
 output "region" {
   value = var.region
 }
-
-#output "service" {
-#  value = module.ssl-certificate.tags["Service"]
-#}
-
-#output "owner" {
-#  value = module.load-balancer.output.tags["Owner"]
-#}
-#
-#output "managed_by" {
-#  value = module.load-balancer.output.tags["Managed_By"]
-#}
-#
-#output "environment" {
-#  value = module.load-balancer.output.tags["Environment"]
-#}
 
--- examples/management/certificates/variables.tf
+++ examples/management/certificates/variables.tf
@@ -19,32 +19,38 @@ variable "logging-preference" {
 }
 
 variable "validation-method" {
-  type    = string
-  default = "DNS"
+  type        = string
+  default     = "DNS"
+  description = "Way AWS certificate is approved"
 }
 
 variable "description" {
-  type    = string
-  default = "description"
+  type        = string
+  default     = "description"
+  description = "used for AWS tagging, Description"
 }
 
 variable "service" {
-  type    = string
-  default = "service"
+  type        = string
+  default     = "service"
+  description = "used for AWS tagging, Service"
 }
 
 variable "environment" {
-  type    = string
-  default = "env"
+  type        = string
+  default     = "env"
+  description = "used for AWS tagging, Environment"
 }
 
 variable "owner" {
-  type    = string
-  default = "owner"
+  type        = string
+  default     = "owner"
+  description = "used for AWS tagging, Owner"
 }
 
 variable "managed_by" {
-  type    = string
-  default = "Terraform"
+  type        = string
+  default     = "Terraform"
+  description = "used for AWS tagging, Managed_By"
 }
 
