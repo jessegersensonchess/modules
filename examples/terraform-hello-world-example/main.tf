5f51b2201871e8ddb8c27267c6720e45f99dfbdc
jessegersensonchess <59052632+jessegersensonchess@users.noreply.github.com>
2023-05-18 13:31:19 +0200

    Update hello world

--- examples/terraform-hello-world-example/main.tf
+++ examples/terraform-hello-world-example/main.tf
@@ -4,6 +4,6 @@ terraform {
 
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
 output "hello_world" {
-  value = "World, Hello!"
+  value       = "World, Hello!"
   description = "hello world description"
 }
